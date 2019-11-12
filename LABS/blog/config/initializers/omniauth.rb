11f401ab3ce471282ff33c3d70b7e9520a3ab142
Mmejiaro <mmejiaro@uncc.edu>
2019-11-12 15:49:33 +0000

    Finalize google sign in functionality

--- LABS/blog/config/initializers/omniauth.rb
+++ LABS/blog/config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '833471542873-0ue930k9ifvjhh2lq0ga7qqjptmj2dc4.apps.googleusercontent.com', 'olL373VqOO5qE6HGF_k_DXGo', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '833471542873-1op6460nlkti5qtoeq574b8av9lrade2.apps.googleusercontent.com', 'rlPloaZn41bKVOQrb3x_A9tl', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
